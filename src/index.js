a76eb050fa18f3963209d7056d26ddd331f92d3b
Oleksandr Palamarchuk <palamarchuk.oa93@gmail.com>
2023-04-11 17:15:40 +0300

    added router to index.js

--- src/index.js
+++ src/index.js
@@ -9,10 +9,8 @@ import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/educational-site-test/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
 );
-
-//basename="/educational-site-test/"
